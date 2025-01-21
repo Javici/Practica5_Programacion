d846387f3a1b68beeafb8889a7f1fb6df4c0219a
PCJAVIERL\Usuario <javier9913@gmail.com>
2025-01-21 13:40:32 +0100

    creado constructo

--- Pratica5_fichajes/src/App.java
+++ Pratica5_fichajes/src/App.java
@@ -1,5 +1,9 @@
+import java.util.Date;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        jugador jude = new jugador("Jude Sharp", null, "Mediocentro", 9); 
+        
     }
+
 }
---
+++ Pratica5_fichajes/src/jugador.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class jugador {
+    public String nombre;
+    public Date fecha_nacimiento;
+    public String posicion;
+    public int dorsal;
+    public jugador(String nombre, Date fecha_nacimiento, String posicion, int dorsal){
+        nombre = this.nombre;
+        fecha_nacimiento = this.fecha_nacimiento;
+        posicion = this.posicion;
+        dorsal = this.dorsal;
+    
+        
+    }
+    
+}
