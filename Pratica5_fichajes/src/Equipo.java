334f8cfc4568f5023e132215b61188ea19e4ffe3
PCJAVIERL\Usuario <javier9913@gmail.com>
2025-01-24 14:38:56 +0100

    commit 24/01 se ha hacho clase jugadores, equipo. posiciones, traspasos, dia productivo

---
+++ Pratica5_fichajes/src/Equipo.java
@@ -0,0 +1,18 @@
+public class Equipo {
+    
+    private String Nombre;
+    public String getNombre(){
+        return Nombre;
+    }
+    public void setNombre(String Nombre){
+        Nombre = this.Nombre;
+    }
+
+    private String Abreviatura;
+    public String getAbreviatura(){
+        return Abreviatura;
+    }
+    public void setAbreviatura(String Abreviatura){
+        Abreviatura = this.Abreviatura;
+    }
+}
