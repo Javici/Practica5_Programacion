7ed66163f34f0f766e9c6d01dde4201acda20b10
DESKTOP-U0CUA57\Javier <javier9913@gmail.com>
2025-01-25 18:41:04 +0100

    Hecho el ejercicio 2: añadir datos para trabajar con ellos

--- Pratica5_fichajes/src/App.java
+++ Pratica5_fichajes/src/App.java
@@ -6,11 +6,58 @@ public class App {
         /**
          * Le resto 1 al mes porque estan guardados del 0 al 11 y es mas intuitivo asi
          */
-        Jugador jude = new Jugador("Jude Sharp", FechaNacimiento(1999, (1 - 1), 13), Posiciones.Centrocampista, 9,
-                "Macedonia",
+        Jugador MarkEvans = new Jugador("Mark Evans", FechaNacimiento(1999, (5 - 1), 7), Posiciones.Portero, 1,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador NathanSwift = new Jugador("Nathan Swift", FechaNacimiento(1999, (8 - 1), 20), Posiciones.Defensa, 2,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador JackWallside = new Jugador("Jack Wallside", FechaNacimiento(1999, (10 - 1), 21), Posiciones.Defensa, 3,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador ScottBanyan = new Jugador("Scott Banyan", FechaNacimiento(1999, (7 - 1), 15), Posiciones.Defensa, 6,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador ArcherHawkins = new Jugador("Archer Hawkins", FechaNacimiento(1998, (11 - 1), 30), Posiciones.Defensa,
+                7,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador CalebStonewall = new Jugador("Caleb Stonewall", FechaNacimiento(1998, (8 - 1), 17),
+                Posiciones.Centrocampista, 8,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador ShawnFrost = new Jugador("Shawn Frost", FechaNacimiento(1999, (1 - 1), 7), Posiciones.Delantero, 9,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador AxelBlaze = new Jugador("Axel Blaze", FechaNacimiento(1998, (7 - 1), 10), Posiciones.Delantero, 10,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador AustinHobbes = new Jugador("Austin Hobbes", FechaNacimiento(2000, (9 - 1), 25), Posiciones.Delantero,
+                11,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador JudeSharp = new Jugador("Jude Sharp", FechaNacimiento(1999, (5 - 1), 11), Posiciones.Centrocampista, 14,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador DavidSamford = new Jugador("David Samford", FechaNacimiento(1999, (6 - 1), 17),
+                Posiciones.Centrocampista, 16,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador KevinDragonfly = new Jugador("Kevin Dragonfly", FechaNacimiento(1998, (2 - 1), 26),
+                Posiciones.Delantero, 17,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador XavierFoster = new Jugador("Xavier Foster", FechaNacimiento(1999, (1 - 1), 20),
+                Posiciones.Centrocampista, 18,
+                "Japon",
+                Traspasos.SIN_SOLICITAR);
+        Jugador DarrenLachance = new Jugador("Darren Lachance", FechaNacimiento(1999, (12 - 1), 13), Posiciones.Portero,
+                20,
+                "Japon",
                 Traspasos.SIN_SOLICITAR);
 
-        jude.check();
+        JudeSharp.check();
+        DarrenLachance.check();
     }
 
     public static Date FechaNacimiento(int ano, int mes, int dia) {
