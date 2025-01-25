d3b12324347b7c752f7771af5a2bf3afa6c5fcc5
DESKTOP-U0CUA57\Javier <javier9913@gmail.com>
2025-01-25 14:36:52 +0100

    Añadida la funcion de la fecha de nacimiento

--- Pratica5_fichajes/src/App.java
+++ Pratica5_fichajes/src/App.java
@@ -3,16 +3,21 @@ import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(1996, 10, 18);
-        Date date = cal.getTime();
-        System.out.println(date);
-
-        Jugador jude = new Jugador("Jude Sharp", date, Posiciones.Centrocampista, 9, "Macedonia",
+        /**
+         * Le resto 1 al mes porque estan guardados del 0 al 11 y es mas intuitivo asi
+         */
+        Jugador jude = new Jugador("Jude Sharp", FechaNacimiento(1999, (1 - 1), 13), Posiciones.Centrocampista, 9,
+                "Macedonia",
                 Traspasos.SIN_SOLICITAR);
 
         jude.check();
+    }
+
+    public static Date FechaNacimiento(int ano, int mes, int dia) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(ano, mes, dia);
+        Date date = cal.getTime();
+        return date;
     }
 
 }
