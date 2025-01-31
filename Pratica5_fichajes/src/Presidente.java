e5f4a663c99a60d3f57447d122e1fa95ba485b05
JAVI\javie <juf0001@alu.medac.es>
2025-01-31 14:32:42 +0100

    Añadidos los toSring a todas las clases y rellenados los datos de Presidentes y Entrenadores (Ej4 terminado)

--- Pratica5_fichajes/src/Presidente.java
+++ Pratica5_fichajes/src/Presidente.java
@@ -3,37 +3,45 @@ import java.text.Normalizer.Form;
 public class Presidente {
     private String dni;
 
-    public String getdni(){
+    public String getdni() {
         return dni;
     }
-    public void setdni(String dni){
-        dni=this.dni;
+
+    public void setdni(String dni) {
+        dni = this.dni;
     }
 
     private String nombrePresidente;
 
-    public String getNombrePresidente(){
+    public String getNombrePresidente() {
         return nombrePresidente;
     }
-    public void setNombrePresidente(String nombrePresidente){
-        nombrePresidente=this.nombrePresidente;
+
+    public void setNombrePresidente(String nombrePresidente) {
+        nombrePresidente = this.nombrePresidente;
     }
 
     private Equipo Equipo;
 
-    public Equipo getEquipo(){
+    public Equipo getEquipo() {
         return Equipo;
     }
 
-    public void Equipo(Equipo Equipo){
-        Equipo=this.Equipo;
+    public void Equipo(Equipo Equipo) {
+        Equipo = this.Equipo;
     }
 
-    //constructor
+    // constructor
 
-    public Presidente(String dni, String nombrePresidente, Equipo Equipo){
-        this.dni=dni;
-        this.nombrePresidente=nombrePresidente;
-        this.Equipo=Equipo;
+    public Presidente(String dni, String nombrePresidente, Equipo Equipo) {
+        this.dni = dni;
+        this.nombrePresidente = nombrePresidente;
+        this.Equipo = Equipo;
+    }
+
+    public String toString() {
+        return "Los datos del presidente son:\nNombre: " + this.nombrePresidente + "\nDNI: " + this.dni
+                + "\nEquipo: "
+                + this.Equipo.getNombre() + "\n\n-----------------------------\n";
     }
 }
